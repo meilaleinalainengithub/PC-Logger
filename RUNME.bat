dd2ceebd1d4927612bf180b4d281489068384de8
meilaleinalainengithub <melkerXsievertsson@gmail.com>
2024-02-21 22:46:10 +0100

    RUNNN

--- RUNME.bat
+++ RUNME.bat
@@ -1,7 +1,7 @@
 IF EXIST ".venv" (
     call .venv\Scripts\activate
-    call cmd /k "C:\Users\sieve\OneDrive\Desktop\Coding\Projects\PC-Logger\src\protector.ps1"
-    start /B cmd /k "C:\Users\sieve\OneDrive\Desktop\Coding\Projects\PC-Logger\.venv\Scripts\python.exe C:\Users\sieve\OneDrive\Desktop\Coding\Projects\PC-Logger\src\main_sender.py"
+    call cmd /k "src\protector.ps1"
+    start /B cmd /k "python.exe src\main_sender.py"
 ) ELSE (
     python -m venv .venv
     call .venv\Scripts\activate
