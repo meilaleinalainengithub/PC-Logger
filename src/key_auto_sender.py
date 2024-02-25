cd35f86f92fae6627ec8e9ead1d8a2bff56a55e2
meilaleinalainengithub <melkerXsievertsson@gmail.com>
2024-02-25 21:35:02 +0100

    uhh added plan but i'm eepy so i no no wanna code

--- src/key_auto_sender.py
+++ src/key_auto_sender.py
@@ -1,6 +1,8 @@
 import keyboard, time
 
-while True:
-    key_down = keyboard.on_press()
-    key_up = keyboard.on_release()
-    # i have no fucking idea what i'm doing here i am too tired lmao
+# - Loop for 5 min
+#   - Track button down
+#   - Track time down
+#   - Track button up
+#   - Write to .txt
+#   - Continue
